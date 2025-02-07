4660406763bf0a31a95f3f2eb1ef79972fba27d5
Veerav Naidu <veeravn@users.noreply.github.com>
2025-02-07 14:12:08 -0600

    Update Candy.java

--- src/main/java/com/vnaidu/Candy.java
+++ src/main/java/com/vnaidu/Candy.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 public class Candy extends Base {
 
     public int candy(int[] ratings) {
+        public int candy(int[] ratings) {
         int n = ratings.length;
         int[] candies = new int[n];
         Arrays.fill(candies, 1);
@@ -18,16 +19,6 @@ public class Candy extends Base {
                 .filter(i -> ratings[i] > ratings[i + 1])
                 .forEach(i -> candies[i] = Math.max(candies[i], candies[i + 1] + 1));
 
-        int totalGas = 0, totalCost = 0;
-
-        int[] gas = {1,2,3,4,5};
-        int[] cost = {3,1,4,5,6};
-        for (int i = 0; i < gas.length; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
         return Arrays.stream(candies).sum();
-
-
     }
 }
