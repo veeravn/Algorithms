ea8f8d6dffdeb351e7cc4c5006100c2647d10aa7
Veerav Naidu <veeravn@gmail.com>
2025-02-11 11:52:24 -0600

    add longest sequence algo

---
+++ src/main/java/com/vnaidu/LongestConsecutiveSequence.java
@@ -0,0 +1,40 @@
+package com.vnaidu;
+
+import java.util.Arrays;
+
+public class LongestConsecutiveSequence extends Base {
+    public static int longestConsecutive(int[] nums) {
+        if(nums.length == 0)
+        {
+            return 0;
+        } else if(nums.length == 1) {
+            return 1;
+        }
+        Arrays.sort(nums);
+
+        int length = 1;
+        int currentLength = 1;
+        for (int i = 1; i< nums.length; i++) {
+            if(nums[i] - nums[i-1] == 1)
+            {
+                currentLength++;
+            } else if(nums[i] - nums[i-1] == 0) {
+                //do nothing
+            } else {
+                if (currentLength > length) {
+                    length = currentLength;
+                }
+                currentLength = 1;
+            }
+        }
+        if (currentLength > length) {
+            length = currentLength;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        logger.info(String.valueOf(longestConsecutive(new int[]{100,4,200,1,3,2})));
+        logger.info(String.valueOf(longestConsecutive(new int[]{})));
+    }
+}
