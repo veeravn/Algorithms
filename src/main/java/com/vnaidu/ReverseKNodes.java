e6435eb87e6170300745176909717fa33ca1b930
Veerav Naidu <veeravn@gmail.com>
2025-02-25 12:27:24 -0600

    reverse linked list nodes between right and left nodes

--- src/main/java/com/vnaidu/ReverseKNodes.java
+++ src/main/java/com/vnaidu/ReverseKNodes.java
@@ -40,7 +40,34 @@ public class ReverseKNodes extends Base {
         return head;
     }
 
-    public static void main (String[] args)
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prevLeft = dummy;
+        ListNode current = head;
+
+        for (int i = 0; i < left - 1; i++) {
+            prevLeft = prevLeft.next;
+            current = current.next;
+        }
+
+        ListNode sublistTail = current;
+        ListNode prev = null;
+
+        for (int i = 0; i < right - left + 1; i++) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        prevLeft.next = prev;
+        sublistTail.next = current;
+
+        return dummy.next;
+
+
+        public static void main (String[] args)
     {
     }
 }
